0c9b62e36493332eaa73f572f6ed1861da0fdf7b
carlw0S <carlosbj1230@gmail.com>
2019-09-25 17:16:21 +0200

    Initial jump + Set fixed timestep to 0.016667

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -11,12 +11,20 @@ public class PlayerScript : MonoBehaviour
     public float quickStepDistance = 2.0f;
     public float quickStepLerpInterpolant = 0.25f;
     public float trailActivation = 0.75f;       // Percentage of maxSpeed that marks when the trail will show.
+    public float baseJumpSpeed = 6.5f;
+    public float jumpReleaseDecceleration = 0.75f;
+    public int shortJumpFrameDuration = 12;
+    private int jumpFrameCounter = 0;
 
     private Rigidbody rb;
     private TrailRenderer trail;
-    private float xSpeed;               // Current horizontal speed.
+    private Vector3 speed;              // Current speed.
+    private float xSpeed,               // Current horizontal speed.
+                  ySpeed;               // Current vertical speed.
     private bool quickStep = false;     // Indicates that the player is performing a quickstep.
     private float quickStepZ;           // Destination in the Z axis of the current quickstep.
+    private bool jumping = false;
+    private bool onGround = true;
 
     void Start()
     {
@@ -27,8 +35,11 @@ public class PlayerScript : MonoBehaviour
 
     void Update()
     {
-        xSpeed = rb.velocity.x;
+        speed = rb.velocity;
+        xSpeed = speed.x;
+        ySpeed = speed.y;
 
+        JumpInput();
         QuickStepInput();
 
         Trail();
@@ -40,6 +51,27 @@ public class PlayerScript : MonoBehaviour
         Jump();
         if (quickStep)
             QuickStep();
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("Ground"))
+        {
+            Vector3 v = speed;
+            v.y = 0;
+            rb.velocity = v;
+
+            onGround = true;
+            jumping = false;
+        }
+    }
+
+    void OnCollisionExit(Collision col)
+    {
+        if (col.gameObject.CompareTag("Ground"))
+        {
+            onGround = false;
+        }
     }
 
 
@@ -54,7 +86,7 @@ public class PlayerScript : MonoBehaviour
                 rb.AddForce(Vector3.right * acceleration, ForceMode.Acceleration);
             else    // Speed cap
             {
-                Vector3 v = rb.velocity;
+                Vector3 v = speed;
                 v.x = maxSpeed;
                 rb.velocity = v;
             }
@@ -67,28 +99,46 @@ public class PlayerScript : MonoBehaviour
                 rb.AddForce(Vector3.left * acceleration, ForceMode.Acceleration);
             else    // Speed cap
             {
-                Vector3 v = rb.velocity;
+                Vector3 v = speed;
                 v.x = -maxSpeed;
                 rb.velocity = v;
             }
         }
         else if (Mathf.Abs(xSpeed) > 0.01f)     // Decceleration when no input
         {
-            Vector3 v = rb.velocity;
+            Vector3 v = speed;
             v.x *= decceleration;
             rb.velocity = v;
         }
         else if (xSpeed != 0)       // Manually set speeds lower than 0.01 to 0 (it keeps getting lower but not null otherwise)
         {
-            Vector3 v = rb.velocity;
+            Vector3 v = speed;
             v.x = 0;
             rb.velocity = v;
         }
     }
 
+    private void JumpInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && onGround)
+        {
+            rb.AddForce(Vector3.up * baseJumpSpeed, ForceMode.VelocityChange);
+
+            onGround = false;
+            jumping = true;
+            jumpFrameCounter = 0;
+        }
+        jumpFrameCounter++;
+    }
+
     private void Jump()
     {
-        
+        if (!Input.GetKey(KeyCode.Space) && jumping && speed.y > 0 && jumpFrameCounter >= shortJumpFrameDuration)    // Salto con distancia mínima?
+        {
+            Vector3 v = speed;
+            v.y *= jumpReleaseDecceleration;
+            rb.velocity = v;
+        }
     }
 
     private void QuickStepInput()
@@ -118,7 +168,7 @@ public class PlayerScript : MonoBehaviour
 
     private void Trail()
     {
-        if (Mathf.Abs(rb.velocity.x) > (maxSpeed * trailActivation))
+        if (Mathf.Abs(xSpeed) > (maxSpeed * trailActivation))
             trail.enabled = true;
         else if (xSpeed == 0)
             trail.enabled = false;
