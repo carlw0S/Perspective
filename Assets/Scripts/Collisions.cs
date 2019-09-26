0bf0ace49da76951d8b0f229c369c8a323876e90
carlw0S <carlosbj1230@gmail.com>
2019-09-27 00:46:01 +0200

    Renamed Collision script

--- Assets/Scripts/Collisions.cs
+++ Assets/Scripts/Collisions.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Collision : MonoBehaviour
+public class Collisions : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
