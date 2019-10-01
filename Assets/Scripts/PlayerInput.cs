d57fe35a01a17544ada9e36e963b19ed9f61ee8c
carlw0S <carlosbj1230@gmail.com>
2019-10-02 00:20:13 +0200

    Implement initial mobile input

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
+    public int inputType = 1;
     public int bufferFrameDuration = 10;
+    public float tiltThreshold = 0.1f;
+    public float swipeMinSpeed = 5.0f;
 
     public bool forward;
     public bool forwardHold;
@@ -22,7 +25,51 @@ public class PlayerInput : MonoBehaviour
 
     
 
+    void Start()
+    {
+        if (Application.platform == RuntimePlatform.Android || Application.platform == RuntimePlatform.IPhonePlayer)
+        {
+            Application.targetFrameRate = 60;   // MOVE SOMEWHERE ELSE EVENTUALLY
+            inputType = 2;
+        }
+    }
+
     void Update()
+    {
+        switch (inputType)
+        {
+            case 1:     KeyboardInput();    break;
+            case 2:     MobileInput();      break;
+            default:    KeyboardInput();    break;
+        }
+
+        Debug.Log(Vector2.SignedAngle(Vector2.right, Vector2.down));
+    }
+
+    void FixedUpdate()
+    {
+        if (bufferedInput != 0 && (bufferFrameCounter++) >= bufferFrameDuration)
+        {
+            CleanBuffer();
+        }
+    }
+
+
+
+    public void CleanBuffer()       // To be called after a buffereable input (mainly the not-hold ones) has been used
+    {
+        bufferedInput = 0;
+    }
+
+
+
+    private void SetBufferedInput(int x)
+    {
+        bufferedInput = x;
+        bufferFrameCounter = 0;
+    }
+
+    private void KeyboardInput()
     {
         if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.D))
         {
@@ -84,26 +131,31 @@ public class PlayerInput : MonoBehaviour
         }
     }
 
-    void FixedUpdate()
+    private void MobileInput()
     {
-        if (bufferedInput != 0 && (bufferFrameCounter++) >= bufferFrameDuration)
+        Vector3 acceleration = Input.acceleration;
+        
+        forwardHold = (acceleration.x > tiltThreshold);
+        backwardHold = (acceleration.x < -tiltThreshold);
+
+        jump = false;
+        quickStepLeft = false;
+        quickStepRight = false;
+        if (Input.touchCount > 0)
         {
-            CleanBuffer();
+            Touch t = Input.GetTouch(0);
+
+            float swipeSpeed = t.deltaPosition.magnitude / t.deltaTime;
+
+            if (swipeSpeed >= swipeMinSpeed)
+            {
+                float swipeAngle = Vector2.SignedAngle(Vector2.right, t.deltaPosition);
+
+                jump = (swipeAngle > 60 && swipeAngle < 120);
+                quickStepLeft = (swipeAngle > 150 || swipeAngle < -150);
+                quickStepRight = (swipeAngle > -30 && swipeAngle < 30);
+            }
+            jumpHold = (t.phase != TouchPhase.Ended);
         }
-    }
-
-
-
-    public void CleanBuffer()       // To be called after a buffereable input (mainly the not-hold ones) has been used
-    {
-        bufferedInput = 0;
-    }
-
-
-
-    private void SetBufferedInput(int x)
-    {
-        bufferedInput = x;
-        bufferFrameCounter = 0;
     }
 }
